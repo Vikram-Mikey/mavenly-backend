52d7b0899e197fb59ed120202d39cc6898002815
Vikram-Mikey <r.vikram.s321@gmail.com>
2025-07-25 14:43:14 +0530

    10 commit

--- website/settings.py
+++ website/settings.py
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
-    "https://mavenly-frontend.vercel.app",
+    "https://mavenly-frontend.vercel.app/",
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = list(default_headers) + [
