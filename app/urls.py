f71e833a492767d2d478328867fffcd32fe1ba28
Vikram-Mikey <r.vikram.s321@gmail.com>
2025-08-04 15:26:44 +0530

    67 commit

--- app/urls.py
+++ app/urls.py
@@ -12,7 +12,6 @@ from .views import (
     RemoveProfilePhotoView,
     SignupView,
     LoginView,
-    ForgotPasswordView,
     CheckoutEmailView,
     PaymentConfirmationEmailView,
     LogoutView,
