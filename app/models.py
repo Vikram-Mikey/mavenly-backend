c7d0f8800e3fef84f7656cdbb9ddf568bf7f2f8f
Vikram-Mikey <r.vikram.s321@gmail.com>
2025-08-05 15:24:30 +0530

    73 commit

--- app/models.py
+++ app/models.py
@@ -36,7 +36,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.email
 
 class ProgramReview(models.Model):
-    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='reviews', null=True, blank=True)
     program = models.CharField(max_length=255)
     rating = models.PositiveSmallIntegerField()
     comment = models.TextField(blank=True)
